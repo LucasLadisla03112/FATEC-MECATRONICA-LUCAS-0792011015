d3c270f03e3028bcb86f49b155fa845b2042e357
LucasLadisla03112 <75044118+LucasLadisla03112@users.noreply.github.com>
2020-11-25 17:31:25 -0300

    Create ola mun.py

---
+++ LTPC-2-2020/Pratica01/ola mun.py
@@ -0,0 +1,11 @@
+# FATEC-MECATRONICA-LUCAS-0792011015
+Projeto da materia de L.T.P.C. II
+  
+print("ola mundo!")
+
+numero1 = int(input("Informe um numero:"))
+numero2 = int(input("Informe outro numero:"))
+
+soma = numero1 + numero2
+
+print(soma)
